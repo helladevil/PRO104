27ca6ff688d8729c6d5392629180a2913cfe26e5
helladevil <127803793+helladevil@users.noreply.github.com>
2024-05-24 21:24:17 -0300

    Add files via upload

---
+++ navigation/MainTabs.js
@@ -0,0 +1,61 @@
+import  React,{useState,useEffect} from 'react';
+import {Text,View,StyleSheet,SafeAreaView,ScrollView} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import Header from '../components/Header';
+import ProfileHeader from '../components/ProfileHeader';
+import HomeScreen from '../screens/Home/HomeScreen';
+import Explore from '../screens/explore/Explore';
+import ProfileScreen from '../screens/Profile/ProfileScreen';
+
+
+export default function MainTabs(){
+
+  const [activeTab,setActiveTab]=useState(false)
+  
+
+
+const switchAndStoreTab=()=>{
+
+
+}
+
+const tabNavigation={
+
+  
+}
+
+
+
+ return(
+     <SafeAreaView>
+     
+     
+     
+     </SafeAreaView>
+ 
+
+ )
+
+}
+
+function selectTab(){
+
+}
+
+
+function selectHeader(){
+
+}
+
+async function storeTab(){
+
+}
+
+async function getTab(){
+
+}
+
+
+
+
